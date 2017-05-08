c7172dd2acc5885a1cd454837da931d325837206
MartinStefanec <martin.stefanec@azet.sk>
2017-05-08 22:01:47 +0200

    commit 2

--- 6.4.4.c
+++ 6.4.4.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<math.h>
 #include<stdlib.h>
+#include<time.h>
 
 typedef struct OWN{
 	float data;
@@ -13,7 +14,7 @@ typedef struct{
 }LOWL;
 
 LOWL *lowl_create_empty (void){
-	LOWL	*list;
+	LOWL *list;
 	list = malloc(sizeof(LOWL));
 	if(list == NULL)
 	return NULL;
@@ -23,8 +24,32 @@ LOWL *lowl_create_empty (void){
 	return list;
 }
 
+LOWL *lowl_create_random(unsigned int size){
+	srand(time(0));
+	int i;
+	LOWL *list;
+	list = malloc(sizeof(LOWL));
+	if(list == NULL)
+	return NULL;
+	for(i=1;i<=size;i++){
+		list->cur=malloc(sizeof(OWN));
+		list->cur->data =rand()%10;
+		if(i<size) 	list->cur->next =list->cur; else list->cur->next=NULL;
+	}
+}
+
+void lowl_destroy(LOWL *list){
+	free(list);
+}
+
+void lowl_print(LOWL *list);
+char lowl_cur_step_left(LOWL *list);
+char lowl_cur_step_right(LOWL *list);
+OWN *lowl_insert_left(LOWL* list, float val);
+OWN *lowl_insert_right(LOWL* list, float val);
+char lowl_delete(LOWL* list);
+
 
 main(){
 	LOWL *line;
-	line=lowl_create_empty();
 }
