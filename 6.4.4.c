47ddc9cfb1d81c93fbb1812e907ee15f6ed509eb
MartinStefanec <martin.stefanec@azet.sk>
2017-06-05 22:23:32 +0200

    added cur stepleft, right and insert right

--- 6.4.4.c
+++ 6.4.4.c
@@ -38,6 +38,8 @@ LOWL *lowl_create_random(unsigned int size){
 //		printf("%f, ",list->cur->data);
 		if(i<size){
 			list->cur->next=malloc(sizeof(OWN));
+			if(list->cur->next == NULL)
+			return NULL;
 			list->cur =list->cur->next; 
 		} 	
 			else 
@@ -61,22 +63,87 @@ void lowl_destroy(LOWL *list){
 
 void lowl_print(LOWL *list){
 	OWN *pomocne;
-	pomocne=list->beg;
-	while(pomocne!=NULL) {
-		printf("%f ",pomocne->data);
-		pomocne=(OWN*) pomocne->next;	
+	
+	printf("\n");
+	if(list->beg==NULL || list->cur==NULL){
+		printf("Empty list");
+	} else{
+		pomocne=list->beg;
+		while(pomocne!=NULL) {
+			if(pomocne==list->cur){
+				printf("Cursor: ");
+			}
+		
+			printf("%f ",pomocne->data);
+			pomocne=(OWN*) pomocne->next;	
+			}
 		}
 }
 
-char lowl_cur_step_left(LOWL *list);
-char lowl_cur_step_right(LOWL *list);
+char lowl_cur_step_left(LOWL *list){
+	OWN *pomocne;
+	
+	if(list->cur==NULL || list->cur==list->beg){
+		printf("\nBOUNDARY_REACHED");
+		return -1;
+	}
+	else{
+		pomocne=list->cur;
+		list->cur=list->beg;
+		do{
+			list->cur=list->cur->next;	
+		} while(
+			list->cur->next!=pomocne
+		);
+		
+		printf("\nLOWL_SUCCESS");
+		return 1;
+	}
+}
+char lowl_cur_step_right(LOWL *list){
+	if(list->cur==NULL || list->cur->next==NULL){
+		printf("\nBOUNDARY_REACHED");
+		return -1;
+	}
+	else{
+		list->cur=list->cur->next;
+		printf("\nLOWL_SUCCESS");
+		return 1;
+	}
+}
 OWN *lowl_insert_left(LOWL* list, float val);
-OWN *lowl_insert_right(LOWL* list, float val);
+OWN *lowl_insert_right(LOWL* list, float val){
+	OWN *pomocne;
+	
+	pomocne=list->cur->next;
+	list->cur->next=malloc(sizeof(OWN));
+	list->cur=list->cur->next;
+	list->cur->data=val;
+	list->cur->next=pomocne;
+}
 char lowl_delete(LOWL* list);
 
 
 main(){
+	int kolko;
+	
+	printf("Zadajte aky dlhy zoznam chcete (ak prazdny, tak 0): ");
+	scanf("%d",&kolko);
+	while(kolko<0){
+		printf("Zadajte aky dlhy zoznam chcete (ak prazdny, tak 0): ");
+		scanf("%d",&kolko);
+	}
+	
 	LOWL *line;
-	line=lowl_create_random(5);
+	if(kolko==0){
+		line=lowl_create_empty();
+	}else{
+		line=lowl_create_random(kolko);
+	}
+	
+	lowl_print(line);
+	lowl_cur_step_left(line);
+	lowl_print(line);
+	lowl_insert_right(line,100);
 	lowl_print(line);
 }
