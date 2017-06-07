3839a2e4f1bd74762f585487c9d74be6eb4e8a57
MartinStefanec <martin.stefanec@azet.sk>
2017-06-07 14:01:59 +0200

    update #11

--- 6.4.4.c
+++ 6.4.4.c
@@ -3,6 +3,11 @@
 #include<stdlib.h>
 #include<time.h>
 
+#define BOUNDARY_REACHED	1
+#define LOWL_SUCCESS	2
+#define LOWL_OK	3
+#define LOWL_FAIL 4
+
 typedef struct OWN{
 	float data;
 	struct OWN *next;
@@ -28,41 +33,52 @@ OWN *lowl_insert_right(LOWL* list, float val){
 	OWN *pomocne;
 	
 	pomocne=malloc(sizeof(OWN));
+	if(pomocne == NULL){
+		return NULL;
+	}
+	
 	if(list->beg==NULL || list->cur==NULL){
 		pomocne->data=val;
 		pomocne->next=NULL;
 		list->beg=pomocne;
 		list->cur=pomocne;
 	}else{
-		pomocne=list->cur->next;
-		list->cur->next=malloc(sizeof(OWN));
-		list->cur=list->cur->next;
-		list->cur->data=val;
-		list->cur->next=pomocne;
+		if(list->cur->next!=NULL){
+			pomocne=list->cur->next;
+			list->cur->next=malloc(sizeof(OWN));
+			if(list->cur->next == NULL){
+				return NULL;
+			}
+		
+			list->cur=list->cur->next;
+			list->cur->data=val;
+			list->cur->next=pomocne;
+		}else{
+			list->cur->next=malloc(sizeof(OWN));
+			if(list->cur->next == NULL){
+				return NULL;
+			}
+		
+			list->cur=list->cur->next;
+			list->cur->data=val;
+			list->cur->next=NULL;	
+		}
+		
 	}
 	
 	return list->cur;
 }
 LOWL *lowl_create_random(unsigned int size){
-	srand(time(0));
 	int i;
 	LOWL *list;
+	
 	list = lowl_create_empty();
 	if(list == NULL){
-		printf("Unable to create list.");
 		return NULL;
 	}
 
-	list->beg=malloc(sizeof(OWN));
-	list->cur=malloc(sizeof(OWN));
-	list->cur=list->beg;
 	for(i=1;i<=size;i++){
-		list->cur->data =rand()%10;
-		if(i<size){
-			lowl_insert_right(list,rand()%10);
-		}else{
-			list->cur->next=NULL;
-		} 
+		lowl_insert_right(list,rand()%10);
 	}
 	
 	return list;
@@ -70,22 +86,23 @@ LOWL *lowl_create_random(unsigned int size){
 void lowl_destroy(LOWL *list){
 	OWN *pomocne;
 	
-	list->cur=list->beg;
-	pomocne=malloc(sizeof(OWN));
-	do {
-		pomocne=list->cur->next;
-		free(list->cur);
-		list->cur=pomocne;
-	}	
-	while(list->cur->next!=NULL);
+	if(list->cur!=NULL && list->beg->next!=NULL){
+		list->cur=list->beg;
+		do {
+			pomocne=list->cur->next;
+			free(list->cur);
+			list->cur=pomocne;
+		}	
+		while(list->cur!=NULL);
+		
+		free(pomocne);
+	}
 	
-	free(pomocne);
 	free(list);
 }
 void lowl_print(LOWL *list){
 	OWN *pomocne;
 	
-	pomocne=malloc(sizeof(OWN));
 	printf("\n");
 	if(list->beg==NULL || list->cur==NULL){
 		printf("Empty list");
@@ -99,59 +116,51 @@ void lowl_print(LOWL *list){
 			printf("%f ",pomocne->data);
 			pomocne=pomocne->next;	
 			}
-		}
-		
-	free(pomocne);
+		}	
 }
 char lowl_cur_step_left(LOWL *list){
 	OWN *pomocne;
 	
-	pomocne=malloc(sizeof(OWN));
 	if(list->cur==NULL || list->cur==list->beg){
-		printf("\nBOUNDARY_REACHED");
-		return -1;
-	}
-	else{
+		return BOUNDARY_REACHED;
+	}else{
 		pomocne=list->cur;
 		list->cur=list->beg;
 		while(list->cur->next!=pomocne){
 			list->cur=list->cur->next;
 		};
 		
-		printf("\nLOWL_SUCCESS");
-		return 1;
+		return LOWL_SUCCESS;
 	}
-	
-	free(pomocne);
 }
 char lowl_cur_step_right(LOWL *list){
 	if(list->cur==NULL || list->cur->next==NULL){
-		printf("\nBOUNDARY_REACHED");
-		return -1;
+		return BOUNDARY_REACHED;
 	}
 	else{
 		list->cur=list->cur->next;
-		printf("\nLOWL_SUCCESS");
-		return 1;
+		return LOWL_SUCCESS;
 	}
 }
 OWN *lowl_insert_left(LOWL* list, float val){
 	OWN *pomocne,*pomocne2;
 	
-	pomocne=malloc(sizeof(OWN));
-	pomocne2=malloc(sizeof(OWN));
 	if(list->beg==NULL || list->cur==NULL){
-		pomocne->data=val;
-		pomocne->next=NULL;
-		list->beg=pomocne;
-		list->cur=pomocne;
+		lowl_insert_right(list,val);
+		return list->cur;
 	}else{
 		if(list->beg==list->cur){
-			pomocne=list->cur;
+			pomocne2=malloc(sizeof(OWN));
+			if(pomocne2 == NULL){
+				return NULL;
+			}
+			
+			pomocne=list->beg;
 			pomocne2->data=val;
 			pomocne2->next=pomocne;
 			list->beg=pomocne2;
 			list->cur=pomocne2;
+			return list->cur;
 		}else{
 			pomocne=list->cur;
 			list->cur=list->beg;
@@ -160,55 +169,44 @@ OWN *lowl_insert_left(LOWL* list, float val){
 			};
 			
 			list->cur->next=malloc(sizeof(OWN));
+			if(list->cur->next == NULL){
+				return NULL;
+			}
+			
 			list->cur=list->cur->next;
 			list->cur->data=val;
 			list->cur->next=pomocne;
+			return list->cur;
 		}
 	}
-	
-	free(pomocne2);
-	return list->cur;
 }
 char lowl_delete(LOWL* list){	
 	OWN *next,*prev,*pomocne;
 	
 	if(list->beg==NULL || list->cur==NULL){
-		printf("\nLOWL_OK");
-		return 0;
+		return LOWL_OK;
 	}else{
 		if(list->beg==list->cur){
 			if(list->cur->next==NULL){
 				list->beg=NULL;
 				list->cur=NULL;
 				list=lowl_create_empty();
-				printf("\nLOWL_OK");
-				return 0;
+				return LOWL_OK;
 			}else{
-				pomocne=malloc(sizeof(OWN));
 				pomocne=list->cur->next;
 				free(list->cur);
 				list->beg=pomocne;
 				list->cur=list->beg;
-				return 0;
+				return LOWL_OK;
 			}	
 		}else{
 			if(list->cur->next==NULL){
-				prev=malloc(sizeof(OWN));
-				pomocne=malloc(sizeof(OWN));
 				pomocne=list->cur;
-				while(list->cur->next!=pomocne){
-					list->cur=list->cur->next;
-				};
-				
-				prev=list->cur;
-				prev->next=NULL;
+				lowl_cur_step_left(list);
+				list->cur->next=NULL;
 				free(pomocne);
-				list->cur=prev;
-				return 0;
+				return LOWL_OK;								
 			}else{
-				next=malloc(sizeof(OWN));
-				prev=malloc(sizeof(OWN));
-				pomocne=malloc(sizeof(OWN));
 				next=list->cur->next;
 				pomocne=list->cur;
 				list->cur=list->beg;
@@ -220,7 +218,7 @@ char lowl_delete(LOWL* list){
 				free(pomocne);
 				prev->next=next;
 				list->cur=next;
-				return 0;
+				return LOWL_OK;
 			}
 		}			
 	}	
@@ -229,40 +227,40 @@ LOWL *lowl_divide(LOWL *list){
 	OWN *pomocne;
 	LOWL *list2;
 	
-	if(list->cur->next==NULL || list->beg==NULL || list->cur==NULL){
-		printf("\nUnable to divide");
-		return NULL;
-	} else {
-		pomocne=malloc(sizeof(OWN));
+	if(list->beg!=NULL || list->cur!=NULL){				
 		pomocne=list->cur->next;	
 		list->cur->next=NULL;
 		list2=lowl_create_empty();
 		list2->beg=pomocne;
 		list2->cur=pomocne;
+		lowl_print(list);
+		lowl_print(list2);
 		return list2;
+	} else {
+		
+		printf("\nUnable to divide");
+		return NULL;
 	}
 	
 }
 
 main(){
-	int kolko,hodnota;
-	LOWL *line,*line2;
+	unsigned int kolko;
+	float hodnota;
+	LOWL *line=NULL,*line2=NULL;
 	int choice;
 	
+	srand(time(0));
 	printf("Zadajte aky dlhy zoznam chcete (ak prazdny, tak 0): ");
 	scanf("%d",&kolko);
 	while(kolko<0){
 		printf("Zadajte aky dlhy zoznam chcete (ak prazdny, tak 0): ");
 		scanf("%d",&kolko);
 	}
-	
-	if(kolko==0){
-		line=lowl_create_empty();
-	}else{
-		line=lowl_create_random(kolko);
-	}
-	
+
+	line=lowl_create_random(kolko);	
 	lowl_print(line);
+	
 	do{
  		printf("\nMenu:  1. cursor right  2. cursor left  3. insert right  4. insert left  5. delete  6. Divide  7. Exit and destroy : ");
  		scanf("%d",&choice);
@@ -275,12 +273,12 @@ main(){
 					lowl_print(line);
 					break;
 			case 3:	printf("Zadajte hotnotu: ");
-					scanf("%d",&hodnota);
+					scanf("%f",&hodnota);
 					lowl_insert_right(line,hodnota);
 					lowl_print(line);
 					break;
 			case 4:	printf("Zadajte hotnotu: ");
-					scanf("%d",&hodnota);
+					scanf("%f",&hodnota);
 					lowl_insert_left(line,hodnota);
 					lowl_print(line);
 					break;
@@ -288,12 +286,10 @@ main(){
 					lowl_print(line);
 					break;
 			case 6:	line2=lowl_divide(line);
-					lowl_print(line);
-					lowl_print(line2);
 					break;
 			case 7:	printf("Goodbye\n");
 					lowl_destroy(line);
-					if(line2->beg!=NULL){
+					if(line2!=NULL){
 						lowl_destroy(line2);
 					}
 					
@@ -301,9 +297,6 @@ main(){
        
 			default: printf("Wrong Choice. Enter again\n");
                     break;
-		} 
-  
+		}  
 	} while (choice != 7);
-	
-	
 }
